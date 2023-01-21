e810920759bca426ef83074aa74b92457cf1b68f
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2023-01-21 21:32:54 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"},
+  
+]
+
+function superbowlWin(record) {
+     let win = record.find((record) => record.result === 'W');
+     if (win){
+        return win.year 
+     }
+     else{
+        return win
+     }
+}
+console.log((superbowlWin(record)))
